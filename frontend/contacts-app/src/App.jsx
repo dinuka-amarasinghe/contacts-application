abc9e4711651b322110a557ab87176d3b211b69a
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-19 02:32:28 +0530

    Initialize contacts fetching and state management in App component

--- frontend/contacts-app/src/App.jsx
+++ frontend/contacts-app/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import {getContacts} from "./api/ContactService.jsx";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div>
-        <h1>Hello</h1>
-    </div>
-  )
+function App() {
+    const [data, setData] = useState({})
+    const [currentPage, setCurrentPage] = useState(0)
+
+    const getAllContacts = async (page= 0, size = 10) => {
+        try {
+            setCurrentPage(page)
+            const {data} = await getContacts(page, size)
+            setData(data)
+            console.log(data)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getAllContacts()
+    }, []);
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
 }
 
 export default App
