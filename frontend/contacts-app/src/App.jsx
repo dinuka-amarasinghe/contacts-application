b27809a256cf4ae31199775b6a68dfa2390e31f2
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-19 02:55:04 +0530

    routing and contact list display

--- frontend/contacts-app/src/App.jsx
+++ frontend/contacts-app/src/App.jsx
@@ -1,14 +1,17 @@
 import {useEffect, useState} from 'react'
-import {getContacts} from "./api/ContactService.jsx";
 import './App.css'
 import Header from "./components/Header.jsx";
-
+import {getContacts} from "./api/ContactService.jsx";
+import ContactList from "./components/ContactList.jsx";
+import {Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
     const [data, setData] = useState({})
     const [currentPage, setCurrentPage] = useState(0)
 
-    const toggleModal = (show) => {}
+    const toggleModal = (show) => {
+        console.log("I was clicked!")
+    }
 
     const getAllContacts = async (page= 0, size = 10) => {
         try {
@@ -29,7 +32,15 @@ function App() {
 
     return (
         <>
-            <Header toggleModal={toggleModal} nbOfContacts={50} />
+            <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+            <main>
+                <div className="container">
+                <Routes>
+                    <Route path="/" element={<Navigate to={'/contacts'}/>}/>
+                    <Route path="/contacts" element={<ContactList data={data} currentPage={currentPage} getAllContacts={getAllContacts} />} />
+                </Routes>
+                </div>
+            </main>
         </>
     )
 }
