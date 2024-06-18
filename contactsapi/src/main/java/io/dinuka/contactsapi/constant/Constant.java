8023ffc83ac78c5dd35bcef04cfca1a2415053c8
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-19 02:41:11 +0530

    Add CORS configuration for Contacts API

--- contactsapi/src/main/java/io/dinuka/contactsapi/constant/Constant.java
+++ contactsapi/src/main/java/io/dinuka/contactsapi/constant/Constant.java
@@ -2,4 +2,5 @@ package io.dinuka.contactsapi.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/Uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
