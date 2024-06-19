294bad2963a34ff8f273f557992b10d054b680ca
dinukah99 <dinukahamarasinghe@gmail.com>
2024-06-19 14:58:25 +0530

    add form state management

--- frontend/contacts-app/src/App.jsx
+++ frontend/contacts-app/src/App.jsx
@@ -1,46 +1,61 @@
-import {useEffect, useState} from 'react'
-import './App.css'
+import {useEffect, useRef, useState} from 'react';
+import './App.css';
 import Header from "./components/Header.jsx";
 import {getContacts} from "./api/ContactService.jsx";
 import ContactList from "./components/ContactList.jsx";
 import {Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
-    const [data, setData] = useState({})
-    const [currentPage, setCurrentPage] = useState(0)
+    const modalRef = useRef();
+    const [data, setData] = useState({});
+    const [currentPage, setCurrentPage] = useState(0);
+    const [file, setFile] = useState(undefined);
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        title: '',
+        status: '',
+    });
 
-    const toggleModal = (show) => {
-        console.log("I was clicked!")
-    }
-
-    const getAllContacts = async (page= 0, size = 10) => {
+    const getAllContacts = async (page = 0, size = 10) => {
         try {
-            setCurrentPage(page)
-            const {data} = await getContacts(page, size)
-            setData(data)
-            console.log(data)
+            setCurrentPage(page);
+            const {data} = await getContacts(page, size);
+            setData(data);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
         }
-        catch (error) {
-            console.log(error)
-        }
-    }
-
+    };
 
     useEffect(() => {
-        getAllContacts()
+        getAllContacts();
     }, []);
+
+    const onChange = (e) => {
+        setValues({...values, [e.target.name]: e.target.value});
+        console.log(values);
+    };
+
+    const toggleModal = (show) => {
+        show ? modalRef.current.showModal() : modalRef.current.close();
+    };
 
     return (
         <>
-            <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+            <Header toggleModal={toggleModal} nbOfContacts={data.totalElements}/>
             <main>
                 <div className="container">
-                <Routes>
-                    <Route path="/" element={<Navigate to={'/contacts'}/>}/>
-                    <Route path="/contacts" element={<ContactList data={data} currentPage={currentPage} getAllContacts={getAllContacts} />} />
-                </Routes>
+                    <Routes>
+                        <Route path="/" element={<Navigate to={'/contacts'}/>}/>
+                        <Route path="/contacts" element={<ContactList data={data} currentPage={currentPage}
+                                                                      getAllContacts={getAllContacts}/>}/>
+                    </Routes>
                 </div>
             </main>
+
             {/* Modal */}
             <dialog ref={modalRef} className="modal" id="modal">
                 <div className="modal__header">
@@ -49,46 +64,47 @@ function App() {
                 </div>
                 <div className="divider"></div>
                 <div className="modal__body">
-                    <form onSubmit={handleNewContact}>
+                    <form>
                         <div className="user-details">
                             <div className="input-box">
                                 <span className="details">Name</span>
-                                <input type="text" value={values.name} onChange={onChange} name='name' required />
+                                <input type="text" value={values.name} onChange={onChange} name="name" required/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Email</span>
-                                <input type="text" value={values.email} onChange={onChange} name='email' required />
+                                <input type="text" value={values.email} onChange={onChange} name="email" required/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Title</span>
-                                <input type="text" value={values.title} onChange={onChange} name='title' required />
+                                <input type="text" value={values.title} onChange={onChange} name="title" required/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Phone Number</span>
-                                <input type="text" value={values.phone} onChange={onChange} name='phone' required />
+                                <input type="text" value={values.phone} onChange={onChange} name="phone" required/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Address</span>
-                                <input type="text" value={values.address} onChange={onChange} name='address' required />
+                                <input type="text" value={values.address} onChange={onChange} name="address" required/>
                             </div>
                             <div className="input-box">
                                 <span className="details">Account Status</span>
-                                <input type="text" value={values.status} onChange={onChange} name='status' required />
+                                <input type="text" value={values.status} onChange={onChange} name="status" required/>
                             </div>
                             <div className="file-input">
                                 <span className="details">Profile Photo</span>
-                                <input type="file" onChange={(event) => setFile(event.target.files[0])} ref={fileRef} name='photo' required />
+                                <input type="file" name="photo" required/>
                             </div>
                         </div>
                         <div className="form_footer">
-                            <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel</button>
-                            <button type='submit' className="btn">Save</button>
+                            <button onClick={() => toggleModal(false)} type="button" className="btn btn-danger">Cancel
+                            </button>
+                            <button type="submit" className="btn">Save</button>
                         </div>
                     </form>
                 </div>
             </dialog>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
